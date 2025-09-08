0a4840bdb86a3e86aa7f3dfbfbdf68b2ff676681
7revor <7revor233@gmail.com>
2025-09-08 19:55:47 +0800

    Update emby_redirect.js

--- emby_redirect.js
+++ emby_redirect.js
@@ -1,10 +1,10 @@
-console.log($environment);
-if ($argument && $network.wifi) {
+const isTV = $environment.system === "tvOS";
+if ($argument && $network.wifi || isTV) {
   const args = $argument.split(",");
   console.log("length:"+args.length)
   if (args.length === 3) {
     const [ssid, source, target] = args;
-    if ($network.wifi.ssid === ssid) {
+    if ($network.wifi.ssid === ssid || isTV) {
       $done({ url: $request.url.replace(source, target) });
     } else {
       $done({});
