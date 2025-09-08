ea0dbe134785a786795986de610c4b69cc8c979c
7revor <7revor233@gmail.com>
2025-09-08 19:45:36 +0800

    Update emby_redirect.js

--- emby_redirect.js
+++ emby_redirect.js
@@ -1,7 +1,14 @@
+console.log($network.wifi);
+console.log($argument);
+console.log($request.url)
 if ($argument && $network.wifi) {
   const args = $argument.split(",");
+  console.log("length:"+args.length)
   if (args.length === 3) {
     const [ssid, source, target] = args;
+    console.log(ssid)
+    console.log(source)
+    console.log(target)
     if ($network.wifi.ssid === ssid) {
       $done({ url: $request.url.replace(source, target) });
     } else {
